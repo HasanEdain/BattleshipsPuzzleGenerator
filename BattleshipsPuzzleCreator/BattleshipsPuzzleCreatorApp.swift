c63803f42923b4711124dadf8d5d3f990cb83893
Hasan Edain <hasane@npcunlimited.com>
2024-09-29 12:59:24 -0700

    Initial Commit

---
+++ BattleshipsPuzzleCreator/BattleshipsPuzzleCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleshipsPuzzleCreatorApp.swift
+//  BattleshipsPuzzleCreator
+//
+//  Created by Hasan Edain on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct BattleshipsPuzzleCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
